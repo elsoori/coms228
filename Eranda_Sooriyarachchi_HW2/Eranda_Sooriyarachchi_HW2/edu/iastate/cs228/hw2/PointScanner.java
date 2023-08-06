7f4c2eaec23781905fbe23d0e9bf9936de4c66d7
Eranda Sooriyarachchi <127037639+elsoori@users.noreply.github.com>
2023-08-06 11:51:14 -0500

    Update PointScanner.java

--- Eranda_Sooriyarachchi_HW2/Eranda_Sooriyarachchi_HW2/edu/iastate/cs228/hw2/PointScanner.java
+++ Eranda_Sooriyarachchi_HW2/Eranda_Sooriyarachchi_HW2/edu/iastate/cs228/hw2/PointScanner.java
@@ -8,11 +8,10 @@ package edu.iastate.cs228.hw2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+import java.io.PrintWriter;
+import java.util.Comparator;
 /**
  *
  * This class sorts all the points in an array of 2D points to determine a reference point whose x and y
@@ -28,11 +27,12 @@ public class PointScanner
 	private Point medianCoordinatePoint;  // point whose x and y coordinates are respectively the medians of
 	// the x coordinates and y coordinates of those points in the array points[].
 	private Algorithm sortingAlgorithm;
+	protected String outputFileName;
+	protected Comparator<Point> pointComparator = null;
+
+	protected long scanTime;  // execution time in ns.
 
 
-	protected long scanTime; 	       // execution time in nanoseconds.
-
-	private String fileName;
 
 
 
@@ -45,15 +45,37 @@ public class PointScanner
 	 */
 	public PointScanner(Point[] pts, Algorithm algo) throws IllegalArgumentException
 	{
-		int n = pts.length;
-		sortingAlgorithm = algo;
-		points = new Point[n];
-
-		for(int i = 0; i < n - 1; i++)
+		if (pts == null || pts.length == 0)
 		{
-			points[i] = pts[i];
+			throw new IllegalArgumentException();
+		}
+		points = new Point[pts.length];
+		for (int i = 0; i < pts.length; i++)
+		{
+			Point p = new Point(pts[i]);
+			points[i] = p;
+		}
+		sortingAlgorithm = algo;
+		if (sortingAlgorithm == Algorithm.InsertionSort)
+		{
+			outputFileName = "insert.txt";
+		}
+		else if (sortingAlgorithm == Algorithm.MergeSort)
+		{
+			outputFileName = "merge.txt";
+		}
+		else if (sortingAlgorithm == Algorithm.QuickSort)
+		{
+			outputFileName = "quick.txt";
+		}
+		else if (sortingAlgorithm == Algorithm.SelectionSort)
+		{
+			outputFileName = "selection.txt";
 		}
 	}
+
+
+
 
 
 	/**
@@ -65,28 +87,54 @@ public class PointScanner
 	 */
 	protected PointScanner(String inputFileName, Algorithm algo) throws FileNotFoundException, InputMismatchException
 	{
-		fileName = inputFileName;
+
 		sortingAlgorithm = algo;
-		ArrayList<Point> temp = new ArrayList<>();
-		File fl = new File(inputFileName);
-		Scanner s = new Scanner(fl);
-
-		while(s.hasNextInt())
+		if (sortingAlgorithm == Algorithm.InsertionSort)
 		{
-			try
-			{
-				temp.add(new Point(s.nextInt(), s.nextInt()));
-
-			}
-			catch(NullPointerException e)
-			{
-				throw new InputMismatchException("input file error");
-			}
+			outputFileName = "insert.txt";
 		}
-		points = new Point[temp.size()];
-		temp.toArray(points);
-		s.close();
+		else if (sortingAlgorithm == Algorithm.MergeSort)
+		{
+			outputFileName = "merge.txt";
+		}
+		else if (sortingAlgorithm == Algorithm.QuickSort)
+		{
+			outputFileName = "quick.txt";
+		}
+		else if (sortingAlgorithm == Algorithm.SelectionSort)
+		{
+			outputFileName = "selection.txt";
+		}
+
+		File file = new File(inputFileName);
+		Scanner scan = new Scanner(file); // Scanner for getting length
+		Scanner sc = new Scanner(file); // Scanner for getting points
+		int numCount = 0;
+		int length = 0;
+		while (scan.hasNextInt())
+		{
+			scan.nextInt();
+			numCount++;
+		}
+		if (numCount % 2 != 0)
+		{
+			scan.close();
+			sc.close();
+			throw new InputMismatchException();
+		}
+		length = numCount / 2;
+		points = new Point[length];
+		for (int i = 0; i < length; i++)
+		{
+			points[i] = new Point(sc.nextInt(), sc.nextInt());
+		}
+		scan.close();
+		sc.close();
 	}
+
+
+
+
 
 
 	/**
@@ -104,43 +152,42 @@ public class PointScanner
 	public void scan()
 	{
 		AbstractSorter aSorter = null;
-
-		if(sortingAlgorithm == Algorithm.InsertionSort)
+		if (sortingAlgorithm == Algorithm.InsertionSort)
 		{
-			InsertionSorter insertion = new InsertionSorter(points);
-			aSorter = insertion;
+			aSorter = new InsertionSorter(points);
 		}
-		if(sortingAlgorithm == Algorithm.MergeSort)
+		else if (sortingAlgorithm == Algorithm.MergeSort)
 		{
-			MergeSorter mergeSort = new MergeSorter(points);
-			aSorter = mergeSort;
+			aSorter = new MergeSorter(points);
 		}
-		if(sortingAlgorithm == Algorithm.QuickSort)
+		else if (sortingAlgorithm == Algorithm.QuickSort)
 		{
-			QuickSorter quickSort = new QuickSorter(points);
-			aSorter = quickSort;
+			aSorter = new QuickSorter(points);
 		}
-		if(sortingAlgorithm == Algorithm.SelectionSort)
+		else if (sortingAlgorithm == Algorithm.SelectionSort)
 		{
-			SelectionSorter selection = new SelectionSorter(points);
-			aSorter = selection;
+			aSorter = new SelectionSorter(points);
 		}
-
-		long startTime = System.nanoTime();
-		for(int i = 0; i < 2; i++) // two rounds of sorting
-		{
-			aSorter.setComparator(i); // call setComparator() with an argument 0 or 1.
-			aSorter.sort(); // call sort().
-			Point medCoord = new Point(); // use a new Point object to store the coordinates of the medianCoordinatePoint
-			medianCoordinatePoint = aSorter.getMedian();
-			medCoord = medianCoordinatePoint; //use a new Point object to store the coordinates of the medianCoordinatePoint
-
-		}
-		long endTime = System.nanoTime();
-		scanTime = endTime - startTime; // sum up the times spent on the two sorting rounds and set the instance variable scanTime
-
-
-
+		Point x1, y1;
+		int x, y;
+		// Sort by x
+		long xStart = System.nanoTime();
+		aSorter.setComparator(0);
+		aSorter.sort();
+		x1 = aSorter.getMedian();
+		x = x1.getX();
+		long xTime = System.nanoTime() - xStart;
+		// Sort by y
+		long yStart = System.nanoTime();
+		aSorter.setComparator(1);
+		aSorter.sort();
+		y1 = aSorter.getMedian();
+		y = y1.getY();
+		long yTime = System.nanoTime() - yStart;
+		// Set median coordinate point
+		medianCoordinatePoint = new Point(x, y);
+		aSorter.getPoints(points);
+		scanTime = xTime + yTime;
 	}
 
 
@@ -157,8 +204,7 @@ public class PointScanner
 	 */
 	public String stats()
 	{
-		String size = ("" + "points.length" + "");
-		return sortingAlgorithm + size + scanTime;
+		return sortingAlgorithm.toString() + " " + points.length + " " + scanTime;
 	}
 
 
@@ -170,7 +216,9 @@ public class PointScanner
 	public String toString()
 	{
 		return "MCP: (" + medianCoordinatePoint.getX() + ", " + medianCoordinatePoint.getY() + ")";
+
 	}
+
 
 
 	/**
@@ -183,24 +231,14 @@ public class PointScanner
 	 */
 	public void writeMCPToFile() throws FileNotFoundException
 	{
-		System.out.print("File name: ");
-		Scanner input = new Scanner(System.in);
-		fileName = input.next();
-		File fl = new File(fileName);
+		File f = new File(outputFileName);
+		PrintWriter pw = new PrintWriter(f);
+		for(Point point : points)
+		{
+			pw.write(point.getX() + ", " + point.getY() + "\n");
+		}
 
-		try
-		{
-			fl.createNewFile();
-			System.out.println("File created: " + fl.getName());
-		}
-		catch(IOException e)
-		{
-			System.out.println("write error");
-			e.printStackTrace();
-		}
-		input.close();
+		pw.write(toString()); //write toString()
+		pw.close();
 	}
-
-
-
 }
